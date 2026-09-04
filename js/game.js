cbc3b5f46d2fb5299d5930d66c96b939b0c2a8c0
landotakman1 <teak.forney25@gmail.com>
2026-09-03 21:44:43 -0500

    Added click loop and live score counter after live server tests look good.

--- js/game.js
+++ js/game.js
@@ -2,9 +2,27 @@ console.log("Bug Squasher Loading...");
 
 const squashBtn = document.getElementById("squash-btn");
 const feedback = document.getElementById("feedback");
+const bugsElement = document.getElementById("bugs-squashed");
+const scoreElement = document.getElementById("score");
 
-if (squashBtn) {
-    squashBtn.addEventListener("click", function () {
-        feedback.textContent = "Where do you see a bug?! Bugs Coming Soon!... Chill";
-    });
+let score = 0;
+let bugsSquashed = 0;
+let pointsPerClick = 1;
+
+function updateDisplay() {
+    scoreElement.textContent = score;
+    bugsElement.textContent = bugsSquashed;
 }
+
+function squashBug() {
+    score = score + pointsPerClick;
+    bugsSquashed++;
+    updateDisplay();
+    feedback.textContent = `Splat! + ${pointsPerClick}`;
+}
+
+
+
+if (squashBtn) {squashBtn.addEventListener("click", squashBug);}
+
+updateDisplay();
