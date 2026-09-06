a00190c75cb07b82500b7fd9364eb5121296103c
landotakman1 <teak.forney25@gmail.com>
2026-09-05 20:22:54 -0500

    Implemented the auto squash upgrade with an interval timer and stacking updates that reduce the timer over time and increase auto squash strength when upgrade lands on multiple of 5.

--- js/game.js
+++ js/game.js
@@ -6,11 +6,18 @@ const bugsElement = document.getElementById("bugs-squashed");
 const scoreElement = document.getElementById("score");
 const strongSquashBtn = document.getElementById("strong-squash");
 const playArena = document.getElementById("play-arena");
+const autoSquashBtn = document.getElementById("auto-squash");
 
 let score = 0;
 let bugsSquashed = 0;
 let pointsPerClick = 1;
 let strongSquashCost = 10;
+let autoSquashCost = 25;
+let autoSquashInterval = 4000;
+let autoSquashLevel = 0;
+let autoSquashStrengthInterval = [5, 10, 15, 20, 25];
+let autoPointsPerClick = 1;
+let autoSquashTimerId = null;
 
 
 function costCheck() {
@@ -18,6 +25,12 @@ function costCheck() {
         strongSquashBtn.disabled = false;
     } else {
         strongSquashBtn.disabled = true;
+    }
+
+    if (score >= autoSquashCost) {
+        autoSquashBtn.disabled = false;
+    } else {
+        autoSquashBtn.disabled = true;
     }
 }
 
@@ -35,11 +48,37 @@ function squashBug() {
     moveBug();
 }
 
+function autoSquash() {
+    score = score + autoPointsPerClick;
+    bugsSquashed++;
+    updateDisplay();
+    feedback.textContent = `Auto Splat! + ${autoPointsPerClick}`;
+    moveBug();
+}
+
 function strongSquashUpgrade() {
     pointsPerClick++;
     score = score - strongSquashCost;
     strongSquashCost = Math.ceil(strongSquashCost * 2.375);
     strongSquashBtn.textContent = `Stronger Click (${Math.ceil(strongSquashCost)} pts)`;
+    updateDisplay();
+}
+
+function startAutoSquashTimer() {
+    if (autoSquashTimerId !== null) {
+        clearInterval(autoSquashTimerId);
+    }
+    autoSquashTimerId = setInterval(autoSquash, autoSquashInterval);
+}
+
+function autoSquashUpgrade() {
+    if (autoSquashLevel >= 1) {autoSquashInterval = autoSquashInterval * 0.95;}
+    if (autoSquashStrengthInterval.includes(autoSquashLevel)) {autoPointsPerClick++}
+    score = score - autoSquashCost;
+    startAutoSquashTimer();
+    autoSquashCost = Math.ceil(autoSquashCost * 3.75);
+    autoSquashBtn.textContent = `Auto Squash (${Math.ceil(autoSquashCost)} pts)`;
+    autoSquashLevel++;
     updateDisplay();
 }
 
@@ -64,3 +103,8 @@ updateDisplay();
 
 if (strongSquashBtn) {strongSquashBtn.addEventListener("click", strongSquashUpgrade);}
 
+updateDisplay();
+
+if (autoSquashBtn) {autoSquashBtn.addEventListener("click", autoSquashUpgrade);}
+
+updateDisplay();
